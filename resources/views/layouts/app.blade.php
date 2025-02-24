344fcdc58a2d48cbbe83a59aa9825dafe48b64b4
gafar_zade <qafarzade2014@gmail.com>
2025-02-24 17:18:50 +0400

    hehe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Fidan-Gafar | Chat') }}</title>
+    <title>{{'Fidan-Gafar | Chat'}}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
